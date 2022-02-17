0d33086f47851fe09a38417fb21e121ad6d009c8
korpukov.daniil <korpukov.daniil@yandex.ru>
2022-02-17 09:06:19 +0300

    Top4ik

---
+++ Передача параметров/MySum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task1
+{
+    class MySum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            int a = Convert.ToInt32(args[0]);
+            int b = Convert.ToInt32(args[1]);
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ Создание класса.Применение коллекций/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Создание_класса.Применение_коллекций
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
